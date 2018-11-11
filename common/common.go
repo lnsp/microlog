b37935f71ec3b63f35963de0f5822781d236b8e0
Lennart Espe <lnsp@users.noreply.github.com>
2018-11-11 21:41:02 +0100

    Move gateway/utils into common package

---
+++ common/common.go
@@ -0,0 +1,37 @@
+package common
+
+import (
+	"github.com/Sirupsen/logrus"
+	"net"
+	"net/http"
+	"os"
+)
+
+type logger struct {
+	logrus.Logger
+}
+
+// WithRequest adds a remote host field to the log entry.
+func (l *logger) WithRequest(req *http.Request) *logrus.Entry {
+	return l.WithField("host", RemoteHost(req))
+}
+
+// RemoteHost returns the remote host IP.
+func RemoteHost(r *http.Request) string {
+	host, _, _ := net.SplitHostPort(r.RemoteAddr)
+	return host
+}
+
+// Logger constructs a new logrus.Logger with additional helper methods.
+func Logger() *logger {
+	log := logrus.Logger{
+		Out:       os.Stderr,
+		Formatter: new(logrus.JSONFormatter),
+		Hooks:     make(logrus.LevelHooks),
+	}
+	if os.Getenv("DEBUG") != "" {
+		log.Level = logrus.DebugLevel
+		log.Formatter = new(logrus.TextFormatter)
+	}
+	return &logger{log}
+}
