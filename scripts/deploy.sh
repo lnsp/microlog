ad9636c51757f2247c262c7f62bd23c4d9b66d9d
Lennart Espe <lnsp@users.noreply.github.com>
2019-05-10 21:16:21 +0200

    Update pull behavior on staging deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,9 +10,11 @@ git fetch --all --tags
 if [ "$STAGE" == "prod" ]; then
     PREFIX=microlog
     git checkout $TAG
+    git pull
 else
     PREFIX=microlog_staging
     git checkout master
+    git pull
 fi
 
 docker-compose up -d -f deployments/docker-compose.yml -f deployments/docker-compose.$STAGE.yml -p $PREFIX --build
