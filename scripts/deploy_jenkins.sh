cdb4852df048a00ed59d2c19e55c38d825911657
Lennart Espe <lnsp@users.noreply.github.com>
2019-06-23 21:18:34 +0200

    Fix deploy_jenkins script

--- scripts/deploy_jenkins.sh
+++ scripts/deploy_jenkins.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker-compose -f docker-compose.yml -f deployments/docker-compose.staging.yml -p $PREFIX up -d --build
+docker-compose -f docker-compose.yml -f deployments/docker-compose.staging.yml -p staging up -d --build
 
 echo "OK."
